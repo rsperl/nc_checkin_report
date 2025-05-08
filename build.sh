26d36b2ff04e548ec274e26a3d367eb457f0970b
Richard Sugg <richard.sugg@sas.com>
2025-05-08 15:54:16 -0400

    update build script

--- build.sh
+++ build.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
+function build {
+    local os=$1
+    local arch=$2
+    local bin_dir="bin/$os/$arch"
+    mkdir -p "$bin_dir"
+    local binary_name="$bin_dir/nc_checkin_report"
+    if [ "$os" == "windows" ]; then
+        binary_name="$binary_name.exe"
+    fi
+    echo "Building for $os/$arch"
+    env GOOS=$os GOARCH=$arch go build -o "$binary_name" *.go
+    echo "Creating tarball for $os/$arch"
+    tar -czvf bin/nc_checkin_report_${os}_${arch}.tar.gz -C "$bin_dir" .
+}
+
 for arch in amd64 arm64; do
     for os in windows darwin; do
         echo "Building for $os/$arch"
@@ -9,7 +24,7 @@ for arch in amd64 arm64; do
         if [ "$os" == "windows" ]; then
             binary_name="$binary_name.exe"
         fi
-        env GOOS=$os GOARCH=$arch go build -o "$binary_name" *.go &
+        build "$os" "$arch" &
     done
 done
 
